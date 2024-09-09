6fc718537d1aea1e87cc98c19052b34fbbc4181c
Gabriel <Gabriel@DESKTOP-R4T3BRK>
2024-09-09 14:08:34 -0300

    Adicionar arquivos de projeto.

---
+++ WebApi/Controllers/ProductsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Config;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : DynamicController<Product>
+    {
+
+        public ProductsController(AppDbContext context) : base(context)    
+        {
+        }
+    }
+}
